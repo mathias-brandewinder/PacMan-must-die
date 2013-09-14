8c77e5429ca1b01a8da98a661796db00501015a5
Preston Guillot <preston.guillot@clear-lines.com>
2013-09-13 19:37:25 -0700

    ghosts more better work

--- PacMan.Xaml/Brains.fs
+++ PacMan.Xaml/Brains.fs
@@ -29,15 +29,32 @@ module Brains =
         let i = rng.Next(0, Set.count choices)
         choices 
         |> Set.toArray 
-        |> fun c -> c.[i]    
-            
+        |> fun c -> c.[i]
+
+    let pacManVisible options lineOfSight =
+        let creaturesInDirection directionVector = directionVector |> List.collect snd
+        let isVulerablePacman (creature: Creature) = match creature with
+                                                           | PacMan(power) -> power < 1
+                                                           | _ -> false
+        let containsPacMan creatures = List.exists isVulerablePacman creatures
+
+        if(Set.contains Up options && lineOfSight.Up |> creaturesInDirection |> containsPacMan) then Some Up
+        else if(Set.contains Left options && lineOfSight.Left |> creaturesInDirection |> containsPacMan) then Some Left
+        else if(Set.contains Down options && lineOfSight.Down |> creaturesInDirection |> containsPacMan) then Some Down
+        else if(Set.contains Right options && lineOfSight.Right |> creaturesInDirection |> containsPacMan) then Some Right
+        else None
+
     let ghostDecision (current: Move) (lineOfSight: Sight) (choices: Move Set) =
-        let restricted = 
-            choices 
+        let restricted =
+            choices
             |> Set.filter (fun c -> not (c = backwards current))
-        if (restricted |> Set.count > 0)
-        then randomMove restricted
-        else randomMove choices
+        let options = if (restricted |> Set.count > 0)
+                      then restricted
+                      else choices
+        let pacDirection = pacManVisible options lineOfSight
+        match pacDirection with
+              | Some move -> move
+              | None -> randomMove options
 
     let pacmanDecision (current: Move) (lineOfSight: Sight) (choices: Move Set) =
         let restricted = 
--- PacMan.Xaml/Ghosts.fs
+++ PacMan.Xaml/Ghosts.fs
@@ -20,27 +20,3 @@ module Ghosts =
         | Down  -> 0<pix>, 1<pix>
         | Left  -> -1<pix>,0<pix>
         | Right -> 1<pix>, 0<pix>
-
-    let pacManVisible options lineOfSight =
-        let creaturesInDirection directionVector = directionVector |> List.collect snd
-        let containsPacMan creatures = match List.tryFind (fun c -> match c with
-                                                                    | int -> true
-                                                                    | _ -> false) creatures with
-                                       | Some value -> true
-                                       | None -> false
-
-        if(Set.contains Up options && lineOfSight.Up |> creaturesInDirection |> containsPacMan) then Some Up
-        else if(Set.contains Left options && lineOfSight.Left |> creaturesInDirection |> containsPacMan) then Some Left
-        else if(Set.contains Down options && lineOfSight.Down |> creaturesInDirection |> containsPacMan) then Some Down
-        else if(Set.contains Right options && lineOfSight.Right |> creaturesInDirection |> containsPacMan) then Some Right
-        else None
-
-        let restricted =
-            choices
-        let options = if (restricted |> Set.count > 0)
-                      then restricted
-                      else choices
-        let pacDirection = pacManVisible options lineOfSight
-        match pacDirection with
-              | Some move -> move
-              | None -> randomMove options
